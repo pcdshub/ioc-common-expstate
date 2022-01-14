34477f6a92523ca0787ec91eaed156e4da8394f1
Ken Lauer <klauer@slac.stanford.edu>
2022-01-13 16:34:42 -0800

    ENH: add caPutLog support

--- iocBoot/templates/st.cmd
+++ iocBoot/templates/st.cmd
@@ -33,9 +33,19 @@ set_savefile_path   ( "$(IOC_DATA)/$$IOCNAME/autosave" )
 set_pass0_restoreFile( "$$IOCNAME.sav" )        #just restore the settings
 set_pass1_restoreFile( "$$IOCNAME.sav" )        #just restore the settings
 
+# Configure access security: this is required for caPutLog.
+asSetFilename("$(TOP)/iocBoot/templates/unrestricted.acf")
 
 # Initialize the IOC and start processing records
 iocInit()
+
+# caPutLogInit("HOST:PORT", config)
+# config options:
+#       caPutLogNone       -1: no logging (disable)
+#       caPutLogOnChange    0: log only on value change
+#       caPutLogAll         1: log all puts
+#       caPutLogAllNoFilter 2: log all puts no filtering on same PV
+caPutLogInit("${EPICS_CAPUTLOG_HOST}:${EPICS_CAPUTLOG_PORT}", 0)
 
 # Start autosave backups
 create_monitor_set( "$$IOCNAME.req", 30, "" )
